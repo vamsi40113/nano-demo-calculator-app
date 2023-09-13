fdb4c29d04e2735bf44d7b6c858b3664f4747e89
vamsi40113 <2000040113ece@gmail.com>
2023-09-13 23:58:58 +0530

    Implemented calculator endpoints

--- server.py
+++ server.py
@@ -1,19 +1,32 @@
 from flask import Flask
-
+from flask import request, jsonify
 app = Flask(__name__)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return 'Hello world!', 200
+
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    data = request.get_json()
+    first = data['first']
+    second = data['second']
+    result = first + second
+    response_data = {'result': result}
+    return jsonify(response_data), 200
+
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    data = request.get_json()
+    first = data['first']
+    second = data['second']
+    result = first - second
+    response_data = {'result': result}
+    return jsonify(response_data), 200
+
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
